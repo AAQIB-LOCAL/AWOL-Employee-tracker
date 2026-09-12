55bdd4cf9ab42edfb612b63301c9133100297f7d
Aaqibhafeez Khan <githubaaqibhafeezkhan@gmail.com>
2026-09-12 14:10:16 +0530

    Remove legacy repository bloat

--- legacy_references/AWOL/app/src/main/java/codex/codex_iter/www/awol/exceptions/InvalidResponseException.java
+++
@@ -1,7 +0,0 @@
-package codex.codex_iter.www.awol.exceptions;
-
-public class InvalidResponseException extends RuntimeException {
-    public InvalidResponseException() {
-        super("Cannot connect to ITER API");
-    }
-}
--- legacy_references/EASAndroid/README.md
+++
@@ -1,4 +0,0 @@
-# EASAndroid,
-Employee Attendance System Android app,
-Database: Firebase Realtime database,
-Check In & Check Out With time & date.
